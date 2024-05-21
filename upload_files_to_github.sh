acd613c646b703e78dc7d7574a453db800cf61e8
Gokul T <mr.tgokul@gmail.com>
2024-05-21 17:37:58 +0530

    Rolling update

--- upload_files_to_github.sh
+++ upload_files_to_github.sh
@@ -2,7 +2,7 @@
 
 # Variables
 REPO_DIR="C:/Users/GT082376/Important/Studies/DevOps/DK/kubernetes-for-containers" # Change this to your local repository path
-COMMIT_MESSAGE="Shell Script Commit" # Customize your commit message
+COMMIT_MESSAGE="Rolling update" # Customize your commit message
 BRANCH_NAME="main" # Change to the branch you want to push to
 
 # Navigate to the repository directory
