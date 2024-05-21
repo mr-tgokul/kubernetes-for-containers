620004b16bcad85961566f53ef7bff7873cac118
Gokul T <mr.tgokul@gmail.com>
2024-05-21 20:44:59 +0530

    Load Balancer

--- upload_files_to_github.sh
+++ upload_files_to_github.sh
@@ -2,7 +2,7 @@
 
 # Variables
 REPO_DIR="C:/Users/GT082376/Important/Studies/DevOps/DK/kubernetes-for-containers" # Change this to your local repository path
-COMMIT_MESSAGE="Node Port" # Customize your commit message
+COMMIT_MESSAGE="Load Balancer" # Customize your commit message
 BRANCH_NAME="main" # Change to the branch you want to push to
 
 # Navigate to the repository directory
