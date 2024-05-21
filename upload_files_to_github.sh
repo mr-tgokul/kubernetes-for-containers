4162c5b0048aee2170497348d18a49f836e8fef9
Gokul T <mr.tgokul@gmail.com>
2024-05-21 20:50:15 +0530

    Cache Volume

--- upload_files_to_github.sh
+++ upload_files_to_github.sh
@@ -2,7 +2,7 @@
 
 # Variables
 REPO_DIR="C:/Users/GT082376/Important/Studies/DevOps/DK/kubernetes-for-containers" # Change this to your local repository path
-COMMIT_MESSAGE="Load Balancer" # Customize your commit message
+COMMIT_MESSAGE="Cache Volume" # Customize your commit message
 BRANCH_NAME="main" # Change to the branch you want to push to
 
 # Navigate to the repository directory
